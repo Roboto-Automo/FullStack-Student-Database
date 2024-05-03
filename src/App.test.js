dab6d8718677d0f77ba0364e95805186bc9d41a7
Roberto De Spirito <robertodespirito@Robertos-MacBook-Air.local>
2024-05-03 17:50:13 +0100

    add form submission and api call test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
-import Home from './Pages/Home';
 import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 
 test('Renders Title', () => {
@@ -55,4 +55,65 @@ test('Renders elements based on admin state', () => {
  
    // Check if the Email input field is not visible after admin button click
    expect(emailInput).not.toBeInTheDocument();
+});
+
+
+// Mock the fetch function
+global.fetch = jest.fn();
+
+test('Creates new entry on form submission', async () => {
+  // Render the component
+  render(<App />, {wrapper: BrowserRouter})
+
+  // Fill out form inputs
+  const nameInput = screen.getByPlaceholderText('Name');
+  const ageInput = screen.getByPlaceholderText('Age');
+  const dobInput = screen.getByPlaceholderText('Date of Birth');
+  const emailInput = screen.getByPlaceholderText('Email');
+
+  userEvent.type(nameInput, 'Jimmy');
+  userEvent.type(ageInput, '25');
+  userEvent.type(dobInput, '1999-01-01');
+  userEvent.type(emailInput, 'john.doe@example.com');
+
+  // Mock the fetch API to resolve with a success response
+  fetch.mockResolvedValueOnce({
+    ok: true,
+    json: async () => ({}), // Mock response data if needed
+  });
+
+  const submitButton = screen.getByText('Add Student');
+  fireEvent.click(submitButton);
+
+  // Wait for the updated data to be fetched and displayed
+  await waitFor(() => {
+    const updatedStudentElement = screen.getByText('Jimmy');
+    expect(updatedStudentElement).toBeInTheDocument();
+  });
+
+  await waitFor(() => {
+    const updatedElement = screen.getByText('1999-01-01');
+    expect(updatedElement).toBeInTheDocument();
+  });
+
+  await waitFor(() => {
+    const updatedElement = screen.getByText('john.doe@example.com');
+    expect(updatedElement).toBeInTheDocument();
+  });
+
+
+  await waitFor(() => {
+    const updatedElement = screen.getByText('Age: 25');
+    expect(updatedElement).toBeInTheDocument();
+  });
+
+  // Check if the fetch API was called with the correct data
+  expect(fetch).toHaveBeenCalledWith('/api/students', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: expect.stringContaining('"id"'), // Expect any id value here
+  });
+
 });
