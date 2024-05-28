6aca79fd1b05e6a6d9dfd0d0988a3f81ad687ee5
Automoroboto <roberto@studiodespirito.co.uk>
2024-05-28 15:11:39 +0100

    add relative path

--- express/xapp.js
+++ express/xapp.js
@@ -99,11 +99,11 @@ app.delete('/api/students/:id', async (req, res) => {
 
 //code inserted here below >>>
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 // Handle other routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
 
